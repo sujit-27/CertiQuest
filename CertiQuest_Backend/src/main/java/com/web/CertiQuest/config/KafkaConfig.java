a99e6c8b7b41737c1ee28a35c0a127d7dbaa897e
sujit-27 <161234730+sujit-27@users.noreply.github.com>
2025-10-03 19:37:26 +0530

    Update KafkaConfig.java

--- CertiQuest_Backend/src/main/java/com/web/CertiQuest/config/KafkaConfig.java
+++ CertiQuest_Backend/src/main/java/com/web/CertiQuest/config/KafkaConfig.java
@@ -22,7 +22,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, QuizEvent> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "nozomi.proxy.rlwy.net:41090");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
@@ -36,7 +36,7 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, QuizEvent> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "nozomi.proxy.rlwy.net:41090");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "certiquest-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
