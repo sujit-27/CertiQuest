994655d9aeae135e4b174cd3628b2d364e334772
sujit-27 <161234730+sujit-27@users.noreply.github.com>
2025-10-05 23:20:57 +0530

    Update pointsSlice.js

--- CertiQuest_Frontend/src/features/pointsSlice.js
+++ CertiQuest_Frontend/src/features/pointsSlice.js
@@ -8,7 +8,7 @@ export const fetchUserPoints = createAsyncThunk(
     if (!isSignedIn) return rejectWithValue('Not signed in');
     try {
       const token = await getToken();
-      const response = await axios.get('http://localhost:8080/users/points', {
+      const response = await axios.get('http://certiquest.up.railway.app/users/points', {
         headers: { Authorization: `Bearer ${token}` },
       });
 
