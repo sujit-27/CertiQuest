bfaaab721cf3c999602dd42cb10eeed907de1432
sujit-27 <161234730+sujit-27@users.noreply.github.com>
2025-10-06 22:04:29 +0530

    Update ClerkJwtAuthFilter.java

--- CertiQuest_Backend/src/main/java/com/web/CertiQuest/security/ClerkJwtAuthFilter.java
+++ CertiQuest_Backend/src/main/java/com/web/CertiQuest/security/ClerkJwtAuthFilter.java
@@ -31,11 +31,58 @@ public class ClerkJwtAuthFilter extends OncePerRequestFilter {
     private ClerkJwksProvider jwksProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        // Simply allow all requests through without JWT validation
-        filterChain.doFilter(request, response);
+        if(request.getRequestURI().contains("/webhooks") || request.getRequestURI().contains("/leaderboard") || request.getRequestURI().contains("/download") || request.getRequestURI().equals("/api/quiz/create")|| request.getRequestURI().equals("/users/points")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+        String authHeader = request.getHeader("Authorization");
+
+        if(authHeader == null || !authHeader.startsWith("Bearer")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try{
+            String token = authHeader.substring(7);
+            String[] chunks = token.split("\\.");
+            if(chunks.length < 3){
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid JWT Token format");
+                return;
+            }
+
+            String headerJson = new String(Base64.getUrlDecoder().decode(chunks[0]));
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode headerNode =  mapper.readTree(headerJson);
+
+            if(!headerNode.has("kid")){
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Token header is missing");
+                return;
+            }
+
+            String kid = headerNode.get("kid").asText();
+            PublicKey publicKey = jwksProvider.getPublicKey(kid);
+
+            Claims claims = Jwts.parser()
+                    .verifyWith(publicKey)
+                    .clockSkewSeconds(60)
+                    .requireIssuer(clerkIssuer)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            String clerkId = claims.getSubject();
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(clerkId,null, Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+
+            SecurityContextHolder.getContext().setAuthentication((authenticationToken));
+            filterChain.doFilter(request,response);
+        } catch (Exception e){
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Invalid JWT Token: "+e.getMessage());
+            return;
+        }
+
     }
 }
