a3656e711a9d092f193931e4a80f28984a7b4594
sujit-27 <161234730+sujit-27@users.noreply.github.com>
2025-10-06 20:26:00 +0530

    Update ClerkJwtAuthFilter.java

--- CertiQuest_Backend/src/main/java/com/web/CertiQuest/security/ClerkJwtAuthFilter.java
+++ CertiQuest_Backend/src/main/java/com/web/CertiQuest/security/ClerkJwtAuthFilter.java
@@ -33,7 +33,7 @@ public class ClerkJwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        if(request.getRequestURI().contains("/webhooks") || request.getRequestURI().contains("/leaderboard") || request.getRequestURI().contains("/download")){
+        if(request.getRequestURI().contains("/webhooks") || request.getRequestURI().contains("/leaderboard") || request.getRequestURI().contains("/download") || request.getRequestURI().equals("/api/quiz/create")){
             filterChain.doFilter(request,response);
             return;
         }
