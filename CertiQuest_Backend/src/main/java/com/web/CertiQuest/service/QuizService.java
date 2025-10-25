eef42a41767a8b4c8d22b6184caeb499dec47e30
sujit-27 <161234730+sujit-27@users.noreply.github.com>
2025-10-25 18:29:36 +0530

    Update QuizService.java

--- CertiQuest_Backend/src/main/java/com/web/CertiQuest/service/QuizService.java
+++ CertiQuest_Backend/src/main/java/com/web/CertiQuest/service/QuizService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
-
+import org.springframework.transaction.annotation.Transactional;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.InputStream;
